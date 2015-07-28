4451fa7d7b0795ce9bf92c9c0799ba9602b9a75e
Max McCarty <mvmccart@gmail.com>
2015-07-27 22:13:43 -0400

    added help and default gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,9 @@ var config = require('./gulp.config')();
 
 var port = process.env.PORT || config.defaultPort;
 
+gulp.task('help', $.taskListing);
+gulp.task('default', ['help']);
+
 gulp.task('vet', function() {
     log('vetting js now');
     return gulp
